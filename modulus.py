e5bdf12bc02c902427c078873605e65c2a23e090
rajashree0biswas <136226368+rajashree0biswas@users.noreply.github.com>
2023-06-11 13:23:44 +0530

    Update modulus.py

--- modulus.py
+++ modulus.py
@@ -1,5 +1,6 @@
 #Modulus of first operand relative to the second
 x=int(input("Enter an integer:"))
 y=int(input("Enter another integer:"))
+print("Taking x=",x,"and y=",y)
 print("Modulus of x relative to y:",x,"%",y,"=",x%y)
 print("Modulus of y relative to x:",y,"%",x,"=",y%x)
