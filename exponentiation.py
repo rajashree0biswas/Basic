52f1e4703126138e1c51a44cc9d580c305e15de8
rajashree0biswas <136226368+rajashree0biswas@users.noreply.github.com>
2023-06-11 13:18:33 +0530

    Update exponentiation.py

--- exponentiation.py
+++ exponentiation.py
@@ -1,5 +1,5 @@
 #Exponentiation of two integers
 a=int(input("Enter an integer:"))
 b=int(input("Enter another integer:"))
-print("a raised to power b gives:",a,"**",b,"=",a**b)
-print("b raised to power a gives:",b,"**",a,"=",b**a)
+print(a,"raised to the power",b,"=",a**b)
+print(b,"raised to the power",a,"=",b**a)
