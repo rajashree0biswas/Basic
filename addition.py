94261291bbde9a2bd6d3e26341120d872ff3497c
rajashree0biswas <136226368+rajashree0biswas@users.noreply.github.com>
2023-06-11 13:13:13 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,4 +1,5 @@
 #Addition of two integers
-a=int(input("Enter a number:"))
-b=int(input("Enter another number:"))
+a=int(input("Enter an integer:"))
+b=int(input("Enter another integer:"))
+print("Hence the sum of two integers is:")
 print(a,"+",b,"=",a+b)
