433f90859014589e5f53dd890f9fa602c6d32e36
rajashree0biswas <136226368+rajashree0biswas@users.noreply.github.com>
2023-06-11 13:24:55 +0530

    Update float division.py

--- float division.py
+++ float division.py
@@ -1,5 +1,5 @@
 #Float division between two integers
 n=float(input("Enter an integer:"))
 d=float(input("Enter another integer:"))
-print(n,"//",d,"=",n//d)
-print(d,"//",n,"=",d//n)
+print(n,"/",d,"=",n//d)
+print(d,"/",n,"=",d//n)
